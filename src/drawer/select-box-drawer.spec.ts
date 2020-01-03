dd9b1bcbaa57715c2c9c48b82eed56923a22f7db
Dan Persa <dan.persa@gmail.com>
2020-01-03 11:27:51 +0100

    Rename tests to specs

--- src/drawer/select-box-drawer.spec.ts
+++ src/drawer/select-box-drawer.spec.ts
@@ -2,7 +2,6 @@ import 'jest-canvas-mock';
 import {CanvasSelectBoxDrawer} from './select-box-drawer';
 import {SelectBox} from "./select-box";
 import {VertexDrawer} from './vertex-drawer';
-import Constants from "../constants";
 
 let ctx: CanvasRenderingContext2D;
 let vertexDrawerMock: VertexDrawer;
