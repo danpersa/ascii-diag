a0804038a3492b0d054c4e9015159228c5a91bed
Dan Persa <dan.persa@gmail.com>
2020-01-10 15:24:01 +0100

    Remove log line

--- src/drawers/grid-drawer.ts
+++ src/drawers/grid-drawer.ts
@@ -15,7 +15,6 @@ export class CanvasGridDrawer implements GridDrawer {
 
     draw(grid: Grid) {
         for (let row = 0; row < grid.rows(); ++row) {
-            console.log("Rows " + grid.rows());
             for (let column = 0; column < grid.columns(); ++column) {
                 const cell = grid.cell(row, column);
                 this.cellDrawer.draw(cell);
