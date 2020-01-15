cece19a21293c34eee6d30397f73e5bdb8a3f3f2
Dan Persa <dan.persa@gmail.com>
2020-01-15 18:49:29 +0100

    Remove unused cell methods

--- src/shapes/box-shape.ts
+++ src/shapes/box-shape.ts
@@ -1,30 +1,17 @@
-import {Domain} from "../drawers/cell";
 import {Shape} from "./shape";
-import {ArrayBoxDrawer} from "../drawers/box-drawer";
 import {Box} from "../drawers/box";
-import Cell = Domain.Cell;
 
 export class BoxShape extends Box implements Shape {
     private readonly _id: number;
-    private readonly _cells: Array<Cell>;
     private _editing: boolean = false;
 
     constructor(id: number, topRow: number, leftColumn: number, bottomRow: number, rightColumn: number) {
         super(topRow, leftColumn, bottomRow, rightColumn);
         this._id = id;
-        this._cells = [];
-
-        const boxDrawer = new ArrayBoxDrawer();
-        boxDrawer.draw(new Box(topRow, leftColumn, bottomRow, rightColumn));
-        this._cells = boxDrawer.cells;
     }
 
     id(): number {
         return this._id;
-    }
-
-    cells(): Array<Domain.Cell> {
-        return this._cells;
     }
 
     editing(): boolean {
--- src/shapes/text-shape.ts
+++ src/shapes/text-shape.ts
@@ -1,32 +1,17 @@
-import {Domain} from "../drawers/cell";
-import Cell = Domain.Cell;
 import {Shape} from "./shape";
 import {Text} from "../drawers/text";
 
 export class TextShape extends Text implements Shape {
     private readonly _id: number;
-    private readonly _cells: Array<Cell>;
     private _editing: boolean = false;
 
     constructor(id: number, row: number, column: number, text: string) {
         super(row, column, text);
         this._id = id;
-        this._cells = [];
-        for (let i = 0; i < this.text.length; ++i) {
-            const cellValue = this.text.charAt(i);
-            const cell = Cell.Builder.from(this.row, this.column + i)
-                .text(cellValue)
-                .build();
-            this._cells.push(cell);
-        }
     }
 
     id(): number {
         return this._id;
-    }
-
-    cells(): Array<Domain.Cell> {
-        return this._cells;
     }
 
     editing(): boolean {
