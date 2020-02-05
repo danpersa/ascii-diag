d9b8ac1b0295a89b60dabb9c518c369507990ae0
Dan Persa <dan.persa@gmail.com>
2020-02-05 15:15:14 +0100

    Create ui for changing the config for connectors. Related to #11

--- src/app.tsx
+++ src/app.tsx
@@ -10,7 +10,16 @@ import ToggleButton from '@material-ui/lab/ToggleButton';
 import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 import DiagCanvas from "./diag-canvas";
 import CursorDefault from "mdi-material-ui/CursorDefault";
-import {CheckboxBlankOutline, FormatText, RayStartArrow} from "mdi-material-ui";
+import {
+    ArrowLeft,
+    ArrowRight,
+    CheckboxBlankOutline,
+    CurrentDc,
+    DotsHorizontal,
+    FormatText,
+    Minus,
+    RayStartArrow
+} from "mdi-material-ui";
 import SvgCanvas from "./svg-diag";
 import {LayerService} from "./layer-service";
 import Grid from "./drawers/grid";
@@ -20,6 +29,7 @@ import Constants from "./constants";
 import {ShapeUpdateNotificationService} from "./shape-update-notification-service";
 import {CellToShapeService} from "./cell-to-shape-service";
 import {ConnectorTipDirectionService} from "./connector-tip-direction-service";
+import IconMenu from "./icon-menu";
 
 
 const appStyles = (theme: Theme) => createStyles({
@@ -37,6 +47,18 @@ const appStyles = (theme: Theme) => createStyles({
         marginTop: theme.spacing(1),
     }
 });
+
+const margin = {
+    margin: '10px',
+    display: 'inline-block'
+};
+
+const padding = {
+    paddingTop: '20px',
+    paddingBottom: '50px',
+    paddingLeft: '20px',
+    paddingRight: '20px',
+};
 
 interface AppProps extends WithStyles<typeof appStyles> {
 
@@ -92,26 +114,30 @@ const AppWithStyles = withStyles(appStyles)(
                             <Button color="inherit">Login</Button>
                         </Toolbar>
                     </AppBar>
-                    <AppBar position="static" color="default">
-                        <Toolbar>
-                            <div>
-                                <ToggleButtonGroup value={this.state.currentTool} exclusive onChange={handleToolChange}>
-                                    <ToggleButton value={Tools.select}>
-                                        <CursorDefault/>
-                                    </ToggleButton>
-                                    <ToggleButton value={Tools.text}>
-                                        <FormatText/>
-                                    </ToggleButton>
-                                    <ToggleButton value={Tools.box}>
-                                        <CheckboxBlankOutline/>
-                                    </ToggleButton>
-                                    <ToggleButton value={Tools.connector}>
-                                        <RayStartArrow/>
-                                    </ToggleButton>
-                                </ToggleButtonGroup>
-                            </div>
-                        </Toolbar>
-                    </AppBar>
+                    <Paper>
+                        <ToggleButtonGroup size="large" value={this.state.currentTool} exclusive
+                                           onChange={handleToolChange}
+                                           style={margin}>
+                            <ToggleButton value={Tools.select} style={padding}>
+                                <CursorDefault/>
+                            </ToggleButton>
+                            <ToggleButton value={Tools.text} style={padding}>
+                                <FormatText/>
+                            </ToggleButton>
+                            <ToggleButton value={Tools.box} style={padding}>
+                                <CheckboxBlankOutline/>
+                            </ToggleButton>
+                            <ToggleButton value={Tools.connector} style={padding}>
+                                <RayStartArrow/>
+                            </ToggleButton>
+                        </ToggleButtonGroup>
+                        <IconMenu title="Start" options={["Flat", "Arrow"]} icons={[<Minus/>, <ArrowLeft/>]}/>
+
+                        <IconMenu title="Line Style" options={["continuous", "dashed", "dotted"]}
+                                  icons={[<Minus/>, <CurrentDc/>, <DotsHorizontal/>]}/>
+
+                        <IconMenu title="End" options={["Flat", "Arrow"]} icons={[<Minus/>, <ArrowRight/>]}/>
+                    </Paper>
                     <UIGrid container spacing={2}>
                         <UIGrid item sm={12} md={6}>
                             <Paper className={this.props.classes.paperStyles} style={{overflow: 'auto'}}>
---
+++ src/icon-menu.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import {createStyles, makeStyles, Theme, withStyles} from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import {ListItemIcon} from "@material-ui/core";
+import {SvgIconProps} from "@material-ui/core/SvgIcon/SvgIcon";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            backgroundColor: theme.palette.background.paper,
+            display: 'inline-block',
+        },
+    }),
+);
+
+const listItemStyle = {
+    paddingTop: '5px',
+    paddingBottom: '5px',
+    display: 'inline-block',
+    border: '1px solid #d3d4d5',
+    //marginTop: '10px'
+};
+
+type IconMenuProps = {
+    title: string,
+    options: Array<string>,
+    icons: Array<React.ReactElement<SvgIconProps>>,
+}
+
+export default function IconMenu({options, icons, title}: IconMenuProps) {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+    const handleClickListItem = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuItemClick = (event: React.MouseEvent<HTMLElement>, index: number) => {
+        setSelectedIndex(index);
+        setAnchorEl(null);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <span className={classes.root}>
+            <List aria-label="Line settings">
+                <ListItem
+                    button
+                    style={listItemStyle}
+                    aria-haspopup="true"
+                    aria-controls="lock-menu"
+                    aria-label="when device is locked"
+                    onClick={handleClickListItem}>
+                    <ListItemText primary={title} secondary={icons[selectedIndex]}/>
+                </ListItem>
+            </List>
+            <Menu
+                id="lock-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}>
+                {options.map((option, index) => (
+                    <MenuItem
+                        key={option}
+                        disabled={index === 0}
+                        selected={index === selectedIndex}
+                        onClick={event => handleMenuItemClick(event, index)}>
+                        <ListItemIcon>
+                            {icons[index]}
+                        </ListItemIcon>
+                        {option}
+                    </MenuItem>
+                ))}
+            </Menu>
+        </span>
+    );
+}
